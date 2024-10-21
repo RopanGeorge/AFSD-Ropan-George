6b944db3dc3f13d52ff9079e8c816c2543a3c041
RopanGeorge <ropangeorgeandrei@gmail.com>
2024-10-21 13:33:42 +0300

    Tema2

--- Tema2.py
+++ Tema2.py
@@ -1,1 +1,17 @@
-articol="Ferraari f80 este o masina rosie"
+articol="Ferrari f80 este o masina rosie, verde sau galbena!  "
+lungime=len(articol)
+jumatate=int(lungime/2)
+partea1=articol[:jumatate]
+partea2=articol[jumatate:]
+partea1=partea1.upper()
+partea1=partea1.strip()
+
+partea2=partea2[::-1]
+partea2=partea2.capitalize()
+partea2=partea2.replace(",","").replace(".","").replace("?","").replace("!","")
+
+rezultat =partea1+partea2
+
+print(partea1)
+print(partea2)
+print(rezultat)
